9b3f2f23c96df1592ada79f39325f52d607c26fb
Andrew <94696321+hoomanhorsey@users.noreply.github.com>
2026-08-26 12:42:09 +1000

    chore: clean up unused code for previous API used as template

--- backend/controllers/scoreController.js
+++ backend/controllers/scoreController.js
@@ -1,11 +1,6 @@
 import scoreService from "../services/scoreService.js";
 
 async function saveScore(req, res) {
-  //insert code'
-
-  // add try/catch
-  // add res.json return
-
   //access JSON from req.body
   const name = req.body.name;
   const elapsedTime = parseInt(req.body.elapsedTime);
@@ -28,72 +23,14 @@ async function saveScore(req, res) {
         leaderboard: topTenLeaderboard,
       });
     } else {
-      res.status(200).json({
+      res.status(400).json({
         success: false,
         message: "Username invalid",
       });
     }
   } catch (error) {
     console.error("Error saving score: ", error);
-    res.status(500).json({ success: false, message: "serverdatabase error" });
-  }
-}
-
-async function verifyLocation(req, res) {
-  console.log("verifyfunction is being called");
-  const userX = parseInt(req.query.x);
-  const userY = parseInt(req.query.y);
-
-  try {
-    const matchedCharacterCoordinates = await charactersService.verifyLocation(
-      userX,
-      userY,
-    );
-
-    if (matchedCharacterCoordinates) {
-      res.status(200).json({
-        success: true,
-        coordinates: matchedCharacterCoordinates.coordinates,
-      });
-    } else {
-      res.status(200).json({
-        success: false,
-        message: "No character found at these co-ordinates",
-      });
-    }
-  } catch (error) {
-    console.error("Error verifying location: ", error);
-    res.status(500).json({ success: false, message: "Server error" });
-  }
-}
-
-async function verifyCharacterGuess(req, res) {
-  const selectedCharacter = req.query.selectedCharacter;
-  const userX = parseInt(req.query.x);
-  const userY = parseInt(req.query.y);
-
-  try {
-    const guessResult = await charactersService.verifyCharacterGuess(
-      selectedCharacter,
-      userX,
-      userY,
-    );
-
-    console.log(guessResult);
-    if (guessResult) {
-      res.status(200).json({
-        success: true,
-        characterId: selectedCharacter,
-      });
-    } else {
-      res.status(200).json({
-        success: false,
-        message: "Wrong name for this character",
-      });
-    }
-  } catch (error) {
-    console.error("Error verifying character: ", error);
-    res.status(500).json({ success: false, message: "Server error" });
+    res.status(500).json({ success: false, message: "server database error" });
   }
 }
 
--- backend/services/scoreService.js
+++ backend/services/scoreService.js
@@ -17,42 +17,6 @@ async function addScore(name, elapsedTime) {
   return { topTenLeaderboard, newEntry, rank };
 }
 
-//test this API
-//http://localhost:3000/characters/verify-location?x=399&y=399
-async function verifyLocation(userX, userY) {
-  const characterArray = await charactersRepository.getCharacterArray();
-
-  const matchedCharacter = matchCharacterCoordinates(
-    characterArray,
-    userX,
-    userY,
-  );
-
-  if (matchedCharacter) {
-    return {
-      success: true,
-      coordinates: [matchedCharacter.x, matchedCharacter.y],
-    };
-  }
-  return false;
-}
-
-// verifyCharacterGuess is only called after a character has been found
-// at these coordinates via verifyLocation, so matchedCharacter is guaranteed
-
-// test this API
-// http://localhost:3000/characters/verify-character-guess?selectedCharacter=Paul%20McCartney&x=399&y=399
-async function verifyCharacterGuess(selectedCharacter, userX, userY) {
-  const characterArray = await charactersRepository.getCharacterArray();
-
-  const matchedCharacter = matchCharacterCoordinates(
-    characterArray,
-    userX,
-    userY,
-  );
-  return selectedCharacter === matchedCharacter.name;
-}
-
 export default {
   addScore,
 };
